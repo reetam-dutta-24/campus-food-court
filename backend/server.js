2d8e73025dccf7e59c9614c6a6fc1c2ad849da46
reetam-dutta-24 <rdutta_be23@thapar.edu>
2025-12-03 12:47:28 +0530

    Final working version - DevOps project complete

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const logger = require('./utils/logger');
 const requestLogger = require('./middleware/requestLogger');
 const helmet = require('helmet');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 // Middleware
 app.use(cors());
